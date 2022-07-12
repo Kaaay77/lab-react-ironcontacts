e65ffa13eacc98a1d7a65f196888b39843b4d8b0
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-07-12 21:08:10 +0200

    itineration 4 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import contactsData from "./contacts.json";
+import React, {useState} from "react";
+
+const contacts5 = contactsData.splice(0, 5);
+const contactosSobrantes = contactsData.splice(5);
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [contacts, setContacts] = useState(contacts5)
 
+  const randomContact = (e) => {
+   const random = contactosSobrantes[Math.floor(Math.random()*contactosSobrantes.length)];
+    setContacts(current => [...current, random]);
+  }
+
+  const popularitySort = () =>{
+     const popular = contacts.sort((a, b) => b.popularity - a.popularity)
+     setContacts(popular)
+    }
+
+  const alphabeticSort = () => {
+   const alph =  contacts.sort((a, b) => {
+      if (a.name < b.name) return -1
+      if (a.name > b.name) return 1
+      return 0
+    })
+    setContacts(alph)
+  }
+
+  // eslint-disable-next-line 
+  return <div className="App-contain">
+
+  <h1>IronContacts</h1>
+
+  <button onClick={ () => randomContact() }>
+    Add Random Contact
+  </button>
+  <button onClick={ () => alphabeticSort ()}>
+  alphabetically  
+  </button>
+  <button onClick={ () => popularitySort() }>
+  highest first
+  </button>
+
+            <table>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Won an Oscar</th>
+                <th>Won an Emmy</th>
+              </tr>
+              {contacts.map((contact) => {
+                  return(
+                    <>
+                    <tr>
+                      <td><img src={contact.pictureUrl} alt="" className="imagen"></img></td>
+                      <td>{contact.name}</td>
+                      <td>{contact.popularity.toFixed(2)}</td>
+                      {
+                        contact.wonOscar ? (
+                          <td>🏆</td> 
+                        ) : (
+                          <></>
+                        )
+                      }
+                      {
+                        contact.wonEmmy ? (
+                          <td>🏆</td> 
+                        ) : (
+                          <></>
+                        )
+                      }
+
+                    </tr>
+                    </>
+                  )
+              })}
+              
+            </table>
+  </div>
+
+}
 export default App;
